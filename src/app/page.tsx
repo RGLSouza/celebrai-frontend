88c9008b8d02c856e85884de01d3741c9306402e
Rafael Souza <111069368+RGLSouza@users.noreply.github.com>
2025-11-17 11:45:36 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProdutoCard from "@/componentes/Card_Produto/ProdutoCard";
 import BannerCarousel from "@/componentes/Home/Banner"
 import CategoriasSlider from "@/componentes/Home/Categorias";
 import SuccessModal from "@/componentes/Home/SuccessModal";
-import SearchBar from "@/componentes/Home/searchBar";
+import SearchBar from "@/componentes/Home/SearchBar";
 
 export const metadata: Metadata = {
   title: "Celebraí - Pagina Principal",
